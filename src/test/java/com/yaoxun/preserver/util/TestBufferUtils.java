71b0e587bdc3dc50b9aef2869a95c42e4f97bb5a
loren <loren@loren.com>
2018-08-16 10:54:33 +0800

    简单测试反射性能

--- src/test/java/com/yaoxun/preserver/util/TestBufferUtils.java
+++ src/test/java/com/yaoxun/preserver/util/TestBufferUtils.java
@@ -24,15 +24,17 @@ public class TestBufferUtils {
 	
 	@Test
 	public void testParse() {
-		byte[] bytes = ByteUtils.hexToBytes("aa00831200c8C000000000000000000000000000000000EF55");
-		ByteBuf buf = Unpooled.copiedBuffer(bytes);
 		long start = System.currentTimeMillis();
-		DemoEntity demoEntity = BufferUtils.parse(buf, DemoEntity.class);
+		byte[] bytes = ByteUtils.hexToBytes("aa00831200c8C000000000000000000000000000000000EF55");
+		for(int i=1; i<=10000; i++) {
+			ByteBuf buf = Unpooled.copiedBuffer(bytes);
+			DemoEntity demoEntity = BufferUtils.parse(buf, DemoEntity.class);
+			buf.clear();
+//			System.out.println(buf.readableBytes());
+		}
 		long end = System.currentTimeMillis();
 		System.out.println("耗时：" + (end - start));
-		System.out.println(demoEntity);
-		buf.clear();
-		System.out.println(buf.readableBytes());
+//		System.out.println(demoEntity);
 	}
 	
 }
